084f49ad969a09b837238cb0dfa71ac87e682332
Fernando Gomez <fernandogomez37@yahoo.com>
2015-07-11 23:19:14 -0700

    images are clickable

--- js/script.js
+++ js/script.js
@@ -1,5 +1,103 @@
 $(function (){
 
-console.log('test');
+	/////////////////////////////////
+	// VARIABLES
+	////////////////////////////////
+
+	var board = $('.board');
+	var images = $('.image');
+	var time = $('.time');
+	var score = $('.score')
+	var player1 = 0;
+	var player2 = 0;
+
+	var images_array = [
+		"./images/beckham.jpg", "./images/beckham.jpg",
+		"./images/dempsey.jpg", "./images/dempsey.jpg",
+		"./images/fernandoTorres.jpg", "./images/fernandoTorres.jpg",
+		"./images/ibrahimovic.jpg", "./images/ibrahimovic.jpg",
+		"./images/ikerCasillas.jpg", "./images/ikerCasillas.jpg",
+		"./images/marcelo.jpg", "./images/marcelo.jpg",
+		"./images/messi.jpg", "./images/messi.jpg",
+		"./images/neymar.jpg", "./images/neymar.jpg",
+		"./images/parkChuYong.jpg", "./images/parkChuYong.jpg",
+		"./images/pique.jpg", "./images/pique.jpg",
+		"./images/ronaldo.jpg", "./images/ronaldo.jpg",
+		"./images/wayneRooney.jpg", "./images/wayneRooney.jpg",
+		"./images/chicharito.jpg", "./images/chicharito.jpg"
+	];
+
 	
-})
+
+	/////////////////////////////////
+	// FUNCTIONS
+	////////////////////////////////
+
+	function shuffle(images_array){
+	    for(var j, x, i = images_array.length; i; j = Math.floor(Math.random() * i), x = images_array[--i], images_array[i] = images_array[j], images_array[j] = x);
+	    return images_array;
+	}
+	var shuffledArray = shuffle(images_array);
+
+	////////////////////////////////////
+
+
+
+
+	
+	$('.playerimg').each(function (index) {	
+		$(this).attr('src', shuffledArray[index]);
+		// console.log(index);
+	});
+	images.children().hide();
+	
+	////////////////////////////////////
+	// EventListener
+	////////////////////////////////////
+
+	
+	var imageClicked = false;
+
+	images.on('click', function (e) {
+		$(this).children().show();
+		if(imageClicked){
+			//second image
+			var imageClicked2 = $(this);
+			var previous = imageClicked.find('img').attr('src');
+			var current = $(this).find('img').attr('src');
+			if(previous === current){
+				$(this).children().show();
+			}else{
+				console.log('NO MATCH!!');
+				//flip items back
+				imageClicked.children().fadeOut(2000);
+				imageClicked2.children().fadeOut(2000);
+				imageClicked=true;
+				// console.log('what is this',$(this));
+			}
+
+		} else {
+			//first image
+			imageClicked=$(this);
+		}
+		// console.log(images_array)
+		// imageClicked.push(e.target.innerHTML);
+		
+
+		console.log(imageClicked);
+		// If (click on 2 different pictures) {
+		// 	pictures need to hide
+		// } else if (pictures are the same) {
+		// 	leave them showing on the screen
+		// 	then add a point to the score.
+		// }
+		// if ( !== ) {
+		// 	$(this).children().hide()
+		// } else {
+		// 	console.log("They're equal");
+
+		// }
+		console.log('img clicked');
+	});
+
+});
